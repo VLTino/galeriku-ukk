41cc95e2c161ecb3033690460d671c0b6ed01252
VLTino <valent14206@gmail.com>
2024-03-06 14:40:51 +0700

    userdata

--- resources/views/hal/userdata.blade.php
+++ resources/views/hal/userdata.blade.php
@@ -8,7 +8,7 @@
 @include('layouts.sidebar')
 
 @section('content')
-    <div class="content container mt-5">
+    <div class="content container">
         @if (session('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
